6fc1f251a285c1c4b63f56b76a7a49114f7eabed
DBotThePony <dbotthepony@yandex.ru>
2022-03-24 23:40:36 +0700

    Added config.json instead of hardcoded config

--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
 
 import { WikiDot } from './WikiDot'
+import { promises } from 'fs'
 
-const scpwiki = new WikiDot('scpfoundation.net', 'http://scpfoundation.net', 'K:/storage/scpfoundation.net');
+interface DaemonConfig {
+	base_directory: string
+	wikis: {name: string, url: string}[]
+}
 
 (async function() {
-	await scpwiki.fetchToken()
-	await scpwiki.workLoop()
+	let config: DaemonConfig
+
+	try {
+		config = JSON.parse(await promises.readFile('./config.json', {encoding: 'utf-8'}))
+	} catch(err) {
+		process.stderr.write('config.json is missing or invalid from working directory.')
+		process.exit(1)
+	}
+
+	for (const {name, url} of config.wikis) {
+		const wiki = new WikiDot(name, url, `${config.base_directory}/${name}`)
+		await wiki.fetchToken()
+		await wiki.workLoop()
+	}
 })()
