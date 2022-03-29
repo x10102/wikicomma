3870f88739ceb04c81e1c41945cd07c500e7cac0
DBotThePony <dbotthepony@yandex.ru>
2022-03-29 20:44:49 +0700

    revert last change

--- index.ts
+++ index.ts
@@ -18,19 +18,17 @@ interface DaemonConfig {
 	}
 
 	for (let {name, url} of config.wikis) {
-		(async () => {
-			try {
-				if (url.endsWith('/')) { // Some of wikidot parts don't like double slash
-					url = url.substring(0, url.length - 1)
-				}
-
-				const wiki = new WikiDot(name, url, `${config.base_directory}/${name}`)
-				await wiki.fetchToken()
-				await wiki.workLoop()
-			} catch(err) {
-				console.error(`Fetching wiki ${name} failed`)
-				console.error(err)
+		try {
+			if (url.endsWith('/')) { // Some of wikidot parts don't like double slash
+				url = url.substring(0, url.length - 1)
 			}
-		})()
+
+			const wiki = new WikiDot(name, url, `${config.base_directory}/${name}`)
+			await wiki.fetchToken()
+			await wiki.workLoop()
+		} catch(err) {
+			console.error(`Fetching wiki ${name} failed`)
+			console.error(err)
+		}
 	}
 })()
