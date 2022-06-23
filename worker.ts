039d2542f8737815ec494a3e95f3771250545ed3
Ammon Smith <ammon.i.smith@gmail.com>
2022-06-23 00:23:34 -0400

    Create worker.ts

---
+++ worker.ts
@@ -0,0 +1,31 @@
+import { promisify } from 'util'
+
+let delayMs: number = -1;
+const sleep = promisify(setTimeout)
+
+export function workerDelay() {
+	return sleep(delayMs)
+}
+
+export function setDelayMs(ms?: number) {
+	delayMs = ms || 0;
+}
+
+export function buildWorker(tasks: any[]) {
+	return async function() {
+		while (tasks.length != 0) {
+			await tasks.pop()()
+			await workerDelay()
+		}
+	};
+}
+
+export async function runWorkers(worker: any, count: number) {
+	const jobs = []
+
+	for (let i = 0; i < count; i++) {
+		jobs.push(worker())
+	}
+
+	await Promise.all(jobs)
+}
